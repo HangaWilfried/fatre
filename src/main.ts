fe6d6c53facd42fa4df624e5a506479b8e61463d
hanga.lagoue <wilfriedhanga5@gmail.com>
2024-06-22 20:13:28 +0100

    feat(setup): adjust eslint config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createI18n  } from "vue-i18n";
+import { createI18n } from "vue-i18n";
 import { prepareApis } from "@/utils/gateway";
 
 import App from "./App.vue";
@@ -12,18 +12,18 @@ const app = createApp(App);
 
 prepareApis();
 const getBrowserLanguage = (): string => {
-    const locale = navigator.language.split("-")[0]
-    return locale === "fr" ? locale : "en";
+  const locale = navigator.language.split("-")[0];
+  return locale === "fr" ? locale : "en";
 };
 
 const i18n = createI18n({
-    messages,
-    legacy: false,
-    fallbackLocale: "en",
-    locale: getBrowserLanguage(),
-})
+  messages,
+  legacy: false,
+  fallbackLocale: "en",
+  locale: getBrowserLanguage(),
+});
 
 app.use(createPinia());
-app.use(i18n)
+app.use(i18n);
 app.use(router);
 app.mount("#app");
