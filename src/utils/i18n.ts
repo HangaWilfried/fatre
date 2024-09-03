f2af1aed504e87069a0454434adea49b2ca601e5
hanga.lagoue <hanga.lagoue@lao-sarl.cm>
2024-09-03 08:44:22 +0100

    refactor(contact-seller-7): eslint and prettier format actions

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -3,12 +3,11 @@ import { useI18n } from "vue-i18n";
 type Message = {
   en: {
     [id: string]: string;
-  },
+  };
   fr: {
     [id: string]: string;
-  },
-}
-
+  };
+};
 
 export const useTranslation = (messages?: Message) => {
   const { t } = useI18n({
